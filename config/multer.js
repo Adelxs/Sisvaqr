87a6db6b82d9dc7bca51b89826f7cd87b4d519ec
Adelxs <adelbros1015@gmail.com>
2026-01-17 21:25:21 -0300

    multer modificado

--- config/multer.js
+++ config/multer.js
@@ -1,8 +1,33 @@
-const multer = require('multer');
+/*const multer = require('multer');
 const path = require('path');
 
 const storage = multer.diskStorage({
     destination: 'uploads/reportes/',
+    filename: (req, file, cb) => {
+        const ext = path.extname(file.originalname);
+        cb(null, Date.now() + ext);
+    }
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;*/
+
+
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const uploadDir = path.join(__dirname, '../uploads/reportes');
+
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, uploadDir);
+    },
     filename: (req, file, cb) => {
         const ext = path.extname(file.originalname);
         cb(null, Date.now() + ext);
