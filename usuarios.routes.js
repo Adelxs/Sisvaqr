889aa4a0fb44028b35bcbc079c0786921c93fe47
Adelxs <adelbros1015@gmail.com>
2026-01-19 17:11:06 -0300

    nuevo endpoint appinventor 2314

--- usuarios.routes.js
+++ usuarios.routes.js
@@ -758,7 +758,7 @@ app.get('/usuario/:rut', async (req, res) => {
 
 
 // POST /auth/panel
-app.post('/auth/panel', async (req, res) => {
+/*app.post('/auth/panel', async (req, res) => {
     const { RUT, Contrasena } = req.body;
 
     if (!RUT || !Contrasena) {
@@ -828,7 +828,7 @@ app.post('/auth/panel', async (req, res) => {
             error: 'Error interno del servidor'
         });
     }
-});
+});*/
 
 
     // Aquí puedes agregar PUT, DELETE, GET por código si lo deseas
